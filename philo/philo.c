8c20a56cb57fa41895960a560e27b59a303f73c7
Vlad-PLK <leonpolo365@gmail.com>
2022-12-06 15:50:03 +0100

    philo pthreadcreate

--- philo/philo.c
+++ philo/philo.c
@@ -6,14 +6,26 @@
 /*   By: vpolojie <vpolojie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 13:08:37 by vpolojie          #+#    #+#             */
-/*   Updated: 2022/11/30 13:46:39 by vpolojie         ###   ########.fr       */
+/*   Updated: 2022/12/06 15:49:36 by vpolojie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <pthread.h>
 
+void	*routine()
+{
+	printf("test from thread\n");
+	sleep(3);
+	printf("end of test\n");
+}
+
 int	main(int argc, char **argv)
 {
-	return (0)
+	pthread_t	t1;
+	pthread_create(&t1, NULL, &routine, NULL);
+	pthread_join(t1, NULL);
+	return (0);
 }
